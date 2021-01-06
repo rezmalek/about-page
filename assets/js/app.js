a30c4063b219687feb1ac8d255f007f87a3b57ef
RezMalek <rezmalek7@gmail.com>
2021-01-06 12:04:17 -0800

    active tabs in js

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+const about = document.querySelector('.about');
+const imgs = document.querySelectorAll('.image');
+const btns = document.querySelectorAll('.tab-btn');
+const contents = document.querySelectorAll('.content');
+
+about.addEventListener('click', e => {
+    const id = e.target.dataset.id;
+
+    if(id) {
+        btns.forEach(btn => {
+            btn.classList.remove('active');
+            e.target.classList.add('active');
+        });
+
+        contents.forEach(content => {
+            content.classList.remove('active');
+            const article = document.getElementById(id);
+            article.classList.add('active')
+        })
+    }
+    
+    
+})
